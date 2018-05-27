e68d3f16bbc58ad798708c8fc306d2b23fa5476a
WillMerritt <williammerritt515@gmail.com>
2018-05-27 09:48:34 -0700

    Added to solution

--- single-number.py
+++ single-number.py
@@ -6,17 +6,25 @@
     Date created: 5/27/2018
     Date last modified: 5/27/2018
     Python Version: 3.6
-'''
+    Leetcode Link: https://leetcode.com/problems/single-number/description/
+
+    Given a non-empty array of integers, every element appears twice except for one. Find that single one.  
+    Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+    Example 1:
+    Input: [2,2,1]
+    Output: 1 
+ '''
+
 # Time Complexity: O(n) 
 # Space Complexity : O(n) 
 
-
 class Solution(object):
+    # @param self 
+    # @param nums, List[int]
+    # @return integer 
     def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+
         hash_table = {}
         for i in nums:
             try:
@@ -24,3 +32,5 @@ class Solution(object):
             except:
                 hash_table[i] = 1
         return hash_table.popitem()[0]
+
+
