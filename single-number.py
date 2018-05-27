3626d1d7141c97e61104f09584597a757e8ad9b5
WillMerritt <williammerritt515@gmail.com>
2018-05-27 01:28:01 -0700

    Making first push to github

---
+++ single-number.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+'''
+    File name: single-number.py
+    Author: William Merritt
+    Date created: 5/27/2018
+    Date last modified: 5/27/2018
+    Python Version: 3.6
+'''
+class solution:
+
+    print ("Hello World")
