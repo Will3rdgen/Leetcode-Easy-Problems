e657d2060f77cf546f4c0e201bc2b60c69ce97ef
WillMerritt <williammerritt515@gmail.com>
2018-05-27 01:41:58 -0700

    hash table (dictionary) solution

--- single-number.py
+++ single-number.py
@@ -7,6 +7,20 @@
     Date last modified: 5/27/2018
     Python Version: 3.6
 '''
-class solution:
+# Time Complexity: O(n) 
+# Space Complexity : O(n) 
 
-    print ("Hello World")
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        hash_table = {}
+        for i in nums:
+            try:
+                hash_table.pop(i)
+            except:
+                hash_table[i] = 1
+        return hash_table.popitem()[0]
